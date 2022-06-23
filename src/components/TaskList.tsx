29162f00fbd63051abe50997f87bb662ef131113
DevJpedroASeco <dev.jpedro@gmail.com>
2022-06-23 10:15:07 -0300

    Challenge finallized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -13,17 +13,27 @@ interface Task {
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
+  
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle === '') return 
+
+    const id = Math.floor(Date.now())
+
+    setTasks([...tasks, { id, title: newTaskTitle, isComplete: false }])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id === id ? {...task, isComplete: !task.isComplete} : {...task})
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id != id))
   }
 
   return (
